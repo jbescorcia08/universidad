ed4892baaccd1a7a920ba83b1723977be68790a1
JesusBarrios <jbescorcia@outlook.es>
2022-04-03 13:36:24 -0500

    Operadores y clases Math con metodos sqrt, pow, round, random

--- operadores/src/operadores/Operadores.java
+++ operadores/src/operadores/Operadores.java
@@ -4,6 +4,8 @@
  */
 package operadores;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jesus
@@ -14,7 +16,26 @@ public class Operadores {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // operadores + - * /
+        Scanner entrada = new Scanner(System.in);
+        float numero1, numero2, suma, resta, mult, div, rest;
+        
+        System.out.println("Ingresa un numero");
+        numero1 = entrada.nextFloat();
+        System.out.println("Digita otro numero");
+        numero2 = entrada.nextFloat();
+        
+        suma = numero1 + numero2;
+        resta = numero1 - numero2;
+        mult = numero1 * numero2;
+        div = numero1 / numero2;
+        rest = numero1 % numero2;
+        
+        System.out.println("Este es el resultado de la suma "+suma);
+        System.out.println("Este es el resultado de la resta "+resta);
+        System.out.println("Este es el resultado de la multiplicasion "+mult);
+        System.out.println("Este es el resultado de la Division");
+        System.out.println("Este es el resultado de mod "+rest);
     }
     
 }
