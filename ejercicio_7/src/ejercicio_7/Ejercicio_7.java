56eb69ee2d6e7b15a14bad08660a20962db06528
JesusBarrios <jbescorcia@outlook.es>
2022-04-09 00:23:34 -0500

    Practica de ultimos ejercicios

--- ejercicio_7/src/ejercicio_7/Ejercicio_7.java
+++ ejercicio_7/src/ejercicio_7/Ejercicio_7.java
@@ -4,6 +4,8 @@
  */
 package ejercicio_7;
 
+import java.util.Scanner;
+
 /**
  *
  * @author jesus
@@ -14,7 +16,23 @@ public class Ejercicio_7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /*
+        Construir un programa que, dado  un numero total de horas, devuelve el numero de semanas
+        dias  y horas equivalentes. por ejemplo, dado un total de 1000 horas debe mostrar, 6 dias y 16 horas.
+        */
+        // declaramos la clase scanner para pedir los datos por consola
+        Scanner entrada = new Scanner(System.in);
+        //declaracion de variables
+        int horasTotal, semanas, dias, horas;
+        //pedimos la cantidad de horas
+        System.out.println("Ingresa la cantidad de horas ");
+        horasTotal = entrada.nextInt();
+        //realizamos la operacion
+        semanas = horasTotal / 168;
+        dias = horasTotal % 168 / 24;
+        horas = horasTotal % 24;
+        //imprimimos el resultado
+        System.out.println("\nEl equivalente es "+"\nSemanas "+semanas+"\ndias "+dias+"\nhoras "+horas);
     }
     
 }
