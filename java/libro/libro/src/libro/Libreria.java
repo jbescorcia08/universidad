3830a39346166c4dfd1b110783e52634e3c48de3
Jesus_Barrios <Jbescorcia@outlook.es>
2023-03-16 12:26:18 -0500

    java

--- java/libro/libro/src/libro/Libreria.java
+++ java/libro/libro/src/libro/Libreria.java
@@ -1,21 +1,24 @@
 package libro;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import libro.Libro;
 
 public class Libreria {
+    public static int contLibro = 0;
+
     public static void main(String[] args) {
+        
         String titulo;
         String autor;
         String genero;
         int evaluar;
         int numeroPaginas;
         int opcion;
-        int i = 0;
 
         Scanner sc = new Scanner(System.in);
         Libro libro1 = new Libro();
-        //Libro vectorLibro [] = new Libro[10];
+        Libro vectorlibro[] = new Libro[10];
 
         do {
             System.out.println("1. Agregar libro\n" +
@@ -23,58 +26,74 @@ public class Libreria {
                     "3. Mostrar libro\n" +
                     "4. Libros con evalucion de 5+\n" +
                     "5. Libros con evaluacion de 5-\n" +
-                    "6. Asignar genero del libro:\n"+
-                    "0. Salir.");
+                    "6. Asignar genero del libro:\n" +
+                    "0. Salir.\n");
             opcion = sc.nextInt();
-            switch (opcion){
+            switch (opcion) {
                 case 1:
-                    libro1.agregarLibro(libro1);
+                    System.out.println("Escribe el titulo del libro: ");
+                    titulo = sc.nextLine();
+                    libro1.setTitulo(titulo);
+                    System.out.println("\n");
+
+                    System.out.println("Escribe el autor del libro: ");
+                    autor = sc.nextLine();
+                    libro1.setAutor(autor);
+                    System.out.println("\n");
+
+                    System.out.println("Escribe el genero del libro: ");
+                    genero = sc.nextLine();
+                    libro1.setGenero(genero);
+                    System.out.println("\n");
+
+                    System.out.println("Introduce el numero de paginas: ");
+                    numeroPaginas = sc.nextInt();
+                    libro1.setNumeroPagina(numeroPaginas);
+                    System.out.println("\n");
+
+                    vectorlibro[Libreria.contLibro] = libro1;
+                    Libreria.contLibro = Libreria.contLibro++;
                     System.out.println("Libro agregado !!");
                     break;
                 case 2:
-                // revisar
+                    // revisar
                     System.out.println("Introduce el titulo del libro a evaluar:");
                     titulo = sc.nextLine();
-                    if(libro1.getTitulo().equals(titulo)){
+                    if (libro1.getTitulo().equals(titulo)) {
                         System.out.println("Evalua el libro:");
                         evaluar = sc.nextInt();
                         libro1.evaluarLibro(evaluar);
                         System.out.println("libro evaluado :)");
-                    }else{
+                    } else {
                         System.out.println("El libro no se encuentra :(");
                     }
-                    
+
                     break;
                 case 3:
-                        libro1.mostrarLibros();
+                    for (int i = 0; i < Libreria.contLibro; i++) {
+                        System.out.println(vectorlibro[i].toString());
+                    }
+                    break;
                 case 4:
-                     //pendiente usar algoritmo de ordenamiento 4 y 5 opcion
-                    if (libro1.getEvaluacion() >= 5){
-                        for (int j = 0; j < i; j++) {
-                            System.out.println(libro1.toString());
-                        }
-                    }else {
-                        System.out.println("No hay libros con evaluacion mayor o igual a 5");
+                    // pendiente usar algoritmo de ordenamiento 4 y 5 opcion
+                    Arrays.sort(vectorlibro);
+                    for (int i = 0; i < vectorlibro.length; i++) {
+                        System.out.println(vectorlibro[i].getEvaluacion());
+                        
                     }
                     break;
                 case 5:
-                    if (libro1.getEvaluacion() < 5){
-                        for (int j = 0; j < i; j++) {
-                            System.out.println(libro1.toString());
-                        }
-                    }else {
-                        System.out.println("No hay libros con evaluacion menor a 5");
-                    }
+                    
                     break;
                 case 6:
                     System.out.println("Introduce el titulo del libro, asigana el genero:");
                     genero = sc.nextLine();
-                    if(libro1.getTitulo().equals(genero)){
+                    if (libro1.getTitulo().equals(genero)) {
                         System.out.println("Evalua el libro:");
                         evaluar = sc.nextInt();
                         libro1.setGenero(genero);
                         System.out.println(libro1.toString());
-                    }else{
+                    } else {
                         System.out.println("El libro no se encuentra :(");
                     }
                     break;
@@ -83,8 +102,7 @@ public class Libreria {
                 default:
                     System.out.println("intruccion no valida");
             }
-        }while (opcion != 0);
-
+        } while (opcion != 0);
 
     }
 }
--- java/libro/libro/src/libro/Libro.java
+++ java/libro/libro/src/libro/Libro.java
@@ -1,16 +1,13 @@
 package libro;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class Libro {
     private String titulo;
     private String autor;
     private String genero;
     private int numeroPaginas;
     private int evaluacion;
-    private Libro vectorlibro[] = new Libro[10];
-    public static int contLibro = 0;
+    
+    
 
     public Libro(String titulo, String autor, String genero, int numeroPaginas, int evaluacion) {
         this.titulo = titulo;
@@ -63,40 +60,6 @@ public class Libro {
     public int getEvaluacion() {
         return evaluacion;
     }
-
-    public void agregarLibro(Libro libro) {
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Escribe el titulo del libro: ");
-        titulo = sc.nextLine();
-        libro.setTitulo(titulo);
-
-        System.out.println("Escribe el autor del libro: ");
-        autor = sc.nextLine();
-        libro.setAutor(autor);
-
-        System.out.println("Escribe el genero del libro: ");
-        genero = sc.nextLine();
-        libro.setGenero(genero);
-
-        System.out.println("Introduce el numero de paginas: ");
-        numeroPaginas = sc.nextInt();
-        libro.setNumeroPagina(numeroPaginas);
-
-        vectorlibro[Libro.contLibro] = libro;
-        Libro.contLibro = ++Libro.contLibro;
-    }
-
-    public void mostrarLibros() {
-        for (int i = 0; i < Libro.contLibro; i++) {
-            if (vectorlibro[i] == null) {
-                System.out.println("No hay libros guardados :(");
-            } else {
-                System.out.println(vectorlibro[i].toString());
-            }
-        }
-    }
-
     public void evaluarLibro(int evaluacion) {
 
         if (evaluacion >= 0 && evaluacion <= 10) {
@@ -105,13 +68,6 @@ public class Libro {
             System.out.println("Evalua el libro correctamente :)");
         }
 
-    }
-
-    public void libroEvaluadoMayor() {
-        Arrays.sort(vectorlibro);
-        for (int i = 0; i < vectorlibro.length; i++) {
-            System.out.println(vectorlibro[i].getEvaluacion());
-        }
     }
 
     @Override
@@ -123,5 +79,6 @@ public class Libro {
                 ", numeroPagina=" + numeroPaginas +
                 ", evaluacion=" + evaluacion +
                 '}';
-    }
+    }    
+    
 }
