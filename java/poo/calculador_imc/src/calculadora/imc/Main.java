4168ab2901efd10e178da30c2022b5e8a197e997
Jesus_Barrios <Jbescorcia@outlook.es>
2023-05-26 14:04:15 -0500

    organization files

---
+++ java/poo/calculador_imc/src/calculadora/imc/Main.java
@@ -0,0 +1,13 @@
+
+package calculadora.imc;
+
+public class Main {
+    
+    
+    public static void main(String[] args) {
+        
+    }
+    public static void calcularImc(){
+        
+    }
+}
--- java/poo/calculador_imc/src/calculadora/imc/Persona.java
+++ java/poo/calculador_imc/src/calculadora/imc/Persona.java
@@ -2,10 +2,10 @@
 package calculadora.imc;
 
 public class Persona {
-    private String nombre;
+    private final String nombre;
     private int edad;
     private static  int dni = 0;
-    private char sexo;
+    private final char sexo;
     private int peso;
     private double altura;
 
@@ -19,7 +19,7 @@ public class Persona {
         if(sexo == 'M' && sexo == 'H' ){
             this.sexo = sexo;
         } else {
-            this.sexo = '?';
+            this.sexo = ' '; 
         }
         if(peso > 0){
             this.peso = peso;
@@ -33,10 +33,48 @@ public class Persona {
         }
         
     }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
     
     public String dniCol(){
         Persona.dni++;
         return "DNICOL"+Persona.dni;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Persona{");
+        sb.append("DNI=").append(dniCol());
+        sb.append("nombre=").append(nombre);
+        sb.append(", edad=").append(edad);
+        sb.append(", sexo=").append(sexo);
+        sb.append(", peso=").append(peso);
+        sb.append(", altura=").append(altura);
+        sb.append('}');
+        return sb.toString();
     }
     
     
